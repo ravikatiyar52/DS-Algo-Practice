b4d9265c70a8126572413326806b801083824772
Ravi.Katiyar <ravi.katiyar@naukri.com>
2018-11-27 22:11:36 +0530

    adding new problem will write code later

---
+++ src/main/java/in/ravi/practice/coding_practice/arrays/LeftRotateArray.java
@@ -0,0 +1,46 @@
+/**
+ * @author Ravi.Katiyar
+ *
+ * Creation Date : 27-Nov-2018
+ * Copyright :InfoEdge Ltd
+ * Description : Type description here
+ * Program Specs Referred : -
+ * 
+ *
+ * -------------------------------------------------------------------------------------------------------------------------------------------------------
+ * Revision:       Version      Last Revision Date         Name                Function/Module affected                            Modification Date
+ * 
+ * 0.0.1           0.0.0.1             27-Nov-2018         Ravi.Katiyar                  --  
+ *             
+ * -------------------------------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * 
+ */
+package in.ravi.practice.coding_practice.arrays;
+
+/**
+ * 
+ * @author Ravi.Katiyar
+ *
+ *         Creation Date : 27-Nov-2018
+ * 
+ *         Description : Type description here Program Specs Referred : -
+ * 
+ *
+ *         -------------------------------------------------------------------------------------------------------------------------------------------------------
+ *         Revision: Version Last Revision Date Name Function/Module affected
+ *         Modification Date
+ * 
+ *         0.0.1 0.0.0.1 27-Nov-2018 Ravi.Katiyar --
+ * 
+ *         -------------------------------------------------------------------------------------------------------------------------------------------------------
+ * 
+ *         Write a function rotate(ar[], d, n) that rotates arr[] of size n by d
+ *         elements 
+ *         Given arr[1,2,3,4,5,6,7] 
+ *         Rotation of this array by 2 will make array 
+ *          arr[3,4,5,6,7,1,2]
+ */
+public class LeftRotateArray {
+
+}
