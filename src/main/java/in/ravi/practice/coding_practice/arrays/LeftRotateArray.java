52a225e6e2bada4194782bce714c1d5482f35ebe
Ravi.Katiyar <ravi.katiyar@naukri.com>
2018-11-29 21:44:13 +0530

    leftRotate code using temp Array

--- src/main/java/in/ravi/practice/coding_practice/arrays/LeftRotateArray.java
+++ src/main/java/in/ravi/practice/coding_practice/arrays/LeftRotateArray.java
@@ -43,4 +43,33 @@ package in.ravi.practice.coding_practice.arrays;
  */
 public class LeftRotateArray {
 
+	public static void main(String args[]) {
+
+		int a[] = { 1, 2, 3, 4, 5, 6, 7 };
+		leftRotateUsingTempArray(a, 4);
+
+		for (int element : a) {
+			System.out.print(element + " ");
+		}
+	}
+
+	public static void leftRotateUsingTempArray(int a[], int d) {
+
+		if (d > 0) {
+			int temp[] = new int[d];
+			int i = 0;
+			for (; i < d; i++) {
+				temp[i] = a[i];
+			}
+
+			for (; i < a.length; i++) {
+				a[i - d] = a[i];
+			}
+
+			for (int t = 0, j = a.length - d; j < a.length; j++, t++) {
+				a[j] = temp[t];
+			}
+
+		}
+	}
 }
