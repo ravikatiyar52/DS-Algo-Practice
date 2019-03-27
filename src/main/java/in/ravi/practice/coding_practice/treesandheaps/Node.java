f43c1d9edf084cb01578d2c7820d1ccf2cd69091
Ravi.Katiyar <ravi.katiyar@naukri.com>
2019-03-27 10:29:35 +0530

    adding all the changes and additions so far

--- src/main/java/in/ravi/practice/coding_practice/treesandheaps/Node.java
+++ src/main/java/in/ravi/practice/coding_practice/treesandheaps/Node.java
@@ -2,4 +2,12 @@ package in.ravi.practice.coding_practice.treesandheaps;
 
 public class Node {
 
+	int item;
+	Node left, right;
+
+	public Node(int key) {
+		this.item = key;
+		left = right = null;
+	}
+
 }
